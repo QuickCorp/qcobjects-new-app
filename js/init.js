2ea554fb6221fcf78241fe04708ed8b70a75e99f
Quickcorp <info@quickcorp.cl>
2019-05-03 19:57:49 -0400

    Initial commit

---
+++ js/init.js
@@ -0,0 +1,22 @@
+'use strict';
+CONFIG.set('relativeImportPath', 'js/packages/');
+CONFIG.set('componentsBasePath', 'templates/components/');
+CONFIG.set('delayForReady', 1); // delay to wait before executing the first ready event, it includes imports
+CONFIG.set('preserveComponentBodyTag', false); // don't use <componentBody></componentBody> tag
+CONFIG.set('useConfigService', false); // Load settings from config.json
+CONFIG.set('routingWay','hash');
+CONFIG.set('useSDK',true);
+CONFIG.set('useLocalSDK',false);
+CONFIG.set('tplextension','tpl.html');
+
+/**
+ * Main import sentence.
+ */
+Import('org.quickcorp.custom', function() {
+  //TODO: Implement your code here
+
+});
+
+Ready(function() {
+//TODO: Implement your code here
+});
---
+++ js/packages/org.quickcorp.custom.controllers.js
@@ -0,0 +1,21 @@
+'use strict';
+Package('org.quickcorp.custom.controllers',[
+  Class('Controller1',Controller,{
+    dependencies:[],
+    component:null,
+    _new_:function (o){
+      this.__new__(o);
+      var controller=this;
+      //TODO: Implement
+    }
+  }),
+  Class('Controller2',Controller,{
+    dependencies:[],
+    component:null,
+    _new_:function (o){
+      this.__new__(o);
+      var controller=this;
+      //TODO: Implement
+    }
+  }),
+]);
