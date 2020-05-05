2db76547b7074b3c659c3d2152e323e459a7af9e
Quickcorp <info@quickcorp.cl>
2020-05-05 01:30:47 -0400

    using white logo in header

--- js/packages/org.quickcorp.custom.views.js
+++ js/packages/org.quickcorp.custom.views.js
@@ -1,12 +1,11 @@
 'use strict';
 Package('org.quickcorp.custom.views',[
-  Class('View1',View,{
-    dependencies:[],
-    component:null,
-    _new_:function (o){
-      this.__new__(o);
-      var controller=this;
-      //TODO: Implement
+  Class('CardView',View,{
+    done (){
+      let component = this.component;
+      component.body.style.display="block";
+      component.body.style.width="100px";
+      component.body.style.height="100px";
     }
   }),
   Class('View2',View,{
