a4661cc3d3325e9ecb95f4f2fb9b27425ec1800a
= <=>
2020-06-14 23:14:15 -0400

    maskable icon attribute for manifest

--- sw.js
+++ sw.js
@@ -23,7 +23,7 @@
  * license document, but changing it is not allowed.
 */
 "use strict";
-const version = "0.0.1";
+const version = "0.0.2";
 const appName = "qcobjectsnewapp";
 const cacheName = `qcobjects-app-${appName}-${version}`;
 const start_url = "/?homescreen=1";
